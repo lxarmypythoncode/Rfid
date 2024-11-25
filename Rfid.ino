df1b1891ea35ec786f5385fd7212ec703ff19bde
LxArmy404 <134345790+lxarmypythoncode@users.noreply.github.com>
2024-11-25 22:36:38 +0800

    Initial commit

---
+++ Rfid.ino
@@ -0,0 +1,77 @@
+//Subscribe Channel "Dwi Juliadi KuaumaYadi"
+
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+#define LED_G 3 //define green LED pin
+#define LED_R 2 //define red LED
+#define RELAY 4 //relay pin
+#define BUZZER 6 //buzzer pin
+#define ACCESS_DELAY 5000
+#define DENIED_DELAY 1000
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+ 
+void setup() 
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();          // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+  pinMode(RELAY, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  noTone(BUZZER);
+  digitalWrite(RELAY, HIGH);
+  Serial.println("Put your card to the reader...");
+  Serial.println();
+
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "04 72 58 32 49 63 80") //TUKAR UID INI SESUAI RFIA TAG KALIAN
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    delay(500);
+    digitalWrite(RELAY, LOW);
+    digitalWrite(LED_G, HIGH);
+    delay(ACCESS_DELAY);
+    digitalWrite(RELAY, HIGH);
+    digitalWrite(LED_G, LOW);
+  }
+ 
+ else   {
+    Serial.println(" Access denied");
+    digitalWrite(LED_R, HIGH);
+    tone(BUZZER, 300);
+    delay(DENIED_DELAY);
+    digitalWrite(LED_R, LOW);
+    noTone(BUZZER);
+  }
+}
